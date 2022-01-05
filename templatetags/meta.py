f4348ef3489caf49862db3b79397f531d188c51d
Ajesh <ajesh@prixa.org>
2022-01-05 09:28:12 +0545

    page url to high priority

--- templatetags/meta.py
+++ templatetags/meta.py
@@ -6,12 +6,15 @@ from page_meta.models import MetaForPage, Meta
 register = template.Library() 
 
 @register.simple_tag
-def get_meta(meta=None):
-    if meta != None and (type(meta) == MetaForPage or type(meta) == Meta):
-        return meta
-
+def get_meta(context_meta=None):
+    # meta from page url
+    # HIGH PRIORITY
     meta = MetaForPage.get_meta_from_url()
     if meta:
         return meta
+
+    # context meta
+    if context_meta != None and (type(context_meta) == MetaForPage or type(context_meta) == Meta):
+        return context_meta
     
     return MetaForPage.get_default_meta()
